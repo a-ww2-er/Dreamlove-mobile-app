e4d0ad3449e1c22c90050a2e558de872cbce9e73
a-ww2-er <warhistories101@gmail.com>
2024-09-28 18:47:46 +0300

    initial application setup

---
+++ Application/app/(routes)/onBoarding/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const onBoarding = () => {
+  return (
+    <View>
+      <Text>onBoarding</Text>
+    </View>
+  )
+}
+
+export default onBoarding
+
+const styles = StyleSheet.create({})
